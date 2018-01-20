e2d7b2e66fd24bcf2e23032b84596388df91feaf
Marius Jansen <mariusb.jansen@googlemail.com>
2018-01-20 12:33:56 +0100

    CalculateRMSE must return a VectorXd type

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,8 +11,28 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+  int n_states = estimations[0].size();
+  VectorXd rmse(n_states);
+  rmse.fill(0.0);
+
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0) {
+    cout << "Invalid estimation or ground_truth data" << endl;
+    return rmse;
+  }
+
+  for (unsigned int i = 0; i < estimations.size(); ++i) {
+    VectorXd residual = estimations[i] - ground_truth[i];
+    // coefficient-wise multiplication
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  // calculate the mean
+  rmse = rmse / estimations.size();
+
+  // calculate the sqare root
+  rmse = rmse.array().sqrt();
+
+  // return the result
+  return rmse;
 }
